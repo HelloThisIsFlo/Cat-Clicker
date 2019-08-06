01aa56fd8132d472cdcb4d43593d5c0901ea40a1
Florian Kempenich <Flori@nKempenich.com>
2019-08-06 17:58:37 +0100

    Add admin area

--- src/index.js
+++ src/index.js
@@ -17,13 +17,16 @@ const model = {
       }
     }
     throw `Cat '${catName}' not found!`;
-  }
+  },
+
+  showAdmin: false
 };
 
 const octopus = {
   init() {
     catListView.init();
     catPreviewView.init();
+    adminView.init();
 
     const firstCat = this.getAllCats()[0];
     this.selectCat(firstCat.name);
@@ -32,11 +35,23 @@ const octopus = {
   selectCat(catName) {
     model.currentCat = model.getCat(catName);
     catPreviewView.render();
+    adminView.render();
   },
 
   clickOnCurrentCat() {
     model.currentCat.clicked++;
     catPreviewView.render();
+    adminView.render();
+  },
+
+  updateCurrentCat(newName, newImgUrl, newClicked) {
+    model.currentCat.name = newName;
+    model.currentCat.imgUrl = newImgUrl;
+    model.currentCat.clicked = newClicked;
+
+    catPreviewView.render();
+    catListView.render();
+    adminView.render();
   },
 
   getAllCats() {
@@ -45,6 +60,15 @@ const octopus = {
 
   getCurrentCat() {
     return model.currentCat;
+  },
+
+  shouldDisplayAdminArea() {
+    return model.showAdmin;
+  },
+
+  toggleAdminArea() {
+    model.showAdmin = !model.showAdmin;
+    adminView.render();
   }
 };
 
@@ -121,6 +145,88 @@ const catPreviewView = {
   }
 };
 
+const adminView = {
+  init() {
+    this.adminDiv = document.getElementById("admin");
+    this.adminDiv.classList.add(styles.admin);
+    this.adminDiv.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <button type="button" id="admin-showhide">Admin</button>
+      <form action="#" id="admin-form">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" name="name" id="admin-name" required/>
+        </div>
+        <div>
+          <label for="name">Image URL:</label>
+          <input type="text" name="imgurl" id="admin-imgurl" required/>
+        </div>
+        <div>
+          <label for="name">Clicked:</label>
+          <input type="number" name="clicked" id="admin-clicked" required/>
+        </div>
+
+        <button type="button" id="admin-cancel">Cancel</button>
+        <button type="submit" id="admin-save">Save</button>
+      </form>
+    `
+    );
+
+    this.adminBtn = document.getElementById("admin-showhide");
+    this.form = document.getElementById("admin-form");
+    this.name = document.getElementById("admin-name");
+    this.imgUrl = document.getElementById("admin-imgurl");
+    this.clicked = document.getElementById("admin-clicked");
+    this.save = document.getElementById("admin-save");
+    this.cancel = document.getElementById("admin-cancel");
+
+    this.adminBtn.addEventListener("click", () => {
+      octopus.toggleAdminArea();
+    });
+    this.save.addEventListener("click", e => {
+      const formValid = () => {
+        for (const input of this.form.getElementsByTagName("input")) {
+          if (!input.checkValidity()) return false;
+        }
+        return true;
+      };
+
+      e.preventDefault();
+      if (formValid()) {
+        octopus.updateCurrentCat(
+          this.name.value,
+          this.imgUrl.value,
+          this.clicked.value
+        );
+      }
+    });
+    this.cancel.addEventListener("click", e => {
+      e.preventDefault();
+      this.render();
+    });
+
+    this.render();
+  },
+
+  render() {
+    if (octopus.shouldDisplayAdminArea()) {
+      this.form.style.display = "block";
+
+      const currentCat = octopus.getCurrentCat();
+      this.name.value = currentCat.name;
+      this.imgUrl.value = currentCat.imgUrl;
+      this.clicked.value = currentCat.clicked;
+    } else {
+      this.form.style.display = "none";
+
+      this.name.value = "";
+      this.imgUrl.value = "";
+      this.clicked.value = "";
+    }
+  }
+};
+
 function main() {
   function newElementWithId(tagName, id) {
     const elmnt = document.createElement(tagName);
@@ -132,9 +238,11 @@ function main() {
 
   const previewArea = newElementWithId("div", "preview");
   const catList = newElementWithId("ul", "cat-list");
+  const adminArea = newElementWithId("div", "admin");
 
   app.appendChild(catList);
   app.appendChild(previewArea);
+  app.appendChild(adminArea);
 
   octopus.init();
 }
