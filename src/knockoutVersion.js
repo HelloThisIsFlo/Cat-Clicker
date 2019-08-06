045d82a96b04b1a3f8b8235c0942b80e2cc32a14
Florian Kempenich <Flori@nKempenich.com>
2019-08-06 19:16:16 +0100

    Pass attributes when creating new Cat

--- src/knockoutVersion.js
+++ src/knockoutVersion.js
@@ -1,9 +1,9 @@
 import ko from "knockout";
 import styles from "./styles.scss";
 
-const Cat = function() {
-  this.name = ko.observable("Frank");
-  this.imgUrl = ko.observable("https://placekitten.com/500/400");
+const Cat = function(data) {
+  this.name = ko.observable(data.name);
+  this.imgUrl = ko.observable(data.imgUrl);
   this.clicked = ko.observable(0);
   this.level = ko.computed(function() {
     if (this.clicked() < 5) {
@@ -17,16 +17,17 @@ const Cat = function() {
     }
   }, this);
 
-  this.nicknames = ko.observableArray([
-    "franky",
-    "frakinou",
-    "Miiiiiiister Franky",
-    "Funky Franky"
-  ]);
+  this.nicknames = ko.observableArray(data.nicknames);
 };
 
 const ViewModel = function() {
-  this.currentCat = ko.observable(new Cat());
+  this.currentCat = ko.observable(
+    new Cat({
+      name: "Frank",
+      imgUrl: "https://placekitten.com/500/400",
+      nicknames: ["franky", "frakinou", "Miiiiiiister Franky", "Funky Franky"]
+    })
+  );
   this.incrementClickCount = () => {
     this.currentCat().clicked(this.currentCat().clicked() + 1);
   };
@@ -40,7 +41,9 @@ function init() {
     <div class="${styles.preview}" data-bind="using: currentCat()">
       <h2 class="${styles.name}" data-bind="text: name"></h2>
       <h3 class="${styles.level}" data-bind="text: level"></h3>
-      <img class="${styles.ca }" data-bind="click: $parent.incrementClickCount, attr: {src: imgUrl}" alt="cat" />
+      <img class="${
+        styles.ca
+      }" data-bind="click: $parent.incrementClickCount, attr: {src: imgUrl}" alt="cat" />
       <div class="${styles.counter}" data-bind="text: clicked"></div>
       <ul data-bind="foreach: nicknames">
         <li data-bind="text: $data"></li>
